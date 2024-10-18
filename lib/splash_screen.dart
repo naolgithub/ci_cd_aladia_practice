794eff9c2d0db2239cdc4b35d7546a6e352a8c52
naolgithub <naolcovered@gmail.com>
2024-10-18 22:19:12 +0300

    updated

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,4 @@
-import 'package:aladia_one/aladia.dart';
+import 'package:aladia_one/theme_aladia.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -21,79 +21,82 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
+        MaterialPageRoute(
+          builder: (context) => MaterialApp(
+            themeMode: ThemeMode.system, // Set to system-based theme
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: Colors.grey,
+              scaffoldBackgroundColor:
+                  Colors.white, // Light theme scaffold background
+              textTheme: const TextTheme(
+                bodyLarge: TextStyle(color: Colors.black),
+                bodyMedium: TextStyle(
+                    color: Colors.black), // All text black in light theme
+              ),
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              scaffoldBackgroundColor:
+                  Colors.black, // Dark theme scaffold background
+              textTheme: const TextTheme(
+                bodyLarge: TextStyle(color: Colors.white),
+                bodyMedium: TextStyle(
+                    color: Colors.white), // All text white in dark theme
+              ),
+            ),
+
+            home: const LoginScreen(),
+          ),
+        ),
       );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system, // Set to system-based theme
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.grey,
-        scaffoldBackgroundColor:
-            Colors.white, // Light theme scaffold background
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.black),
-          bodyMedium:
-              TextStyle(color: Colors.black), // All text black in light theme
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
         ),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black, // Dark theme scaffold background
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.white),
-          bodyMedium:
-              TextStyle(color: Colors.white), // All text white in dark theme
-        ),
-      ),
-
-      home: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            color: Colors.transparent,
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // The image takes flexible space to avoid overflow
-                Flexible(
-                  child: Image.asset(
-                    'assets/app_icon.png',
-                  ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // The image takes flexible space to avoid overflow
+              Flexible(
+                child: Image.asset(
+                  'assets/app_icon.png',
                 ),
-                const SizedBox(width: 16),
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 50),
-                    child: Text(
-                      'Aladia',
-                      style: GoogleFonts.sora(
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 5,
-                      ),
+              ),
+              const SizedBox(width: 16),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Text(
+                    'Aladia',
+                    style: GoogleFonts.sora(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 5,
                     ),
                   ),
                 ),
-              ]
-                  .animate()
-                  .tint(
-                      color: const Color.fromARGB(255, 223, 159, 11),
-                      delay: 300.ms,
-                      duration: 1000.ms,
-                      curve: const ElasticInCurve())
-                  .move(duration: 1000.ms),
-            ),
+              ),
+            ]
+                .animate()
+                .tint(
+                    color: const Color.fromARGB(255, 223, 159, 11),
+                    delay: 300.ms,
+                    duration: 1000.ms,
+                    curve: const ElasticInCurve())
+                .move(duration: 1000.ms),
           ),
         ),
       ),
